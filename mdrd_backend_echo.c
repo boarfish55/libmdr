aa0b7971173b0022d9387219a7188afa852ea822
Pascal Lalonde <plalonde@overnet.ca>
2024-07-25 22:09:03 -0400

    POC cert verification in mdrd_backend_echo.c

--- mdrd_backend_echo.c
+++ mdrd_backend_echo.c
@@ -1,9 +1,11 @@
+#include <openssl/err.h>
 #include <openssl/x509.h>
 #include <stdio.h>
 #include <err.h>
 #include <errno.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <syslog.h>
 #include <unistd.h>
 #include "mdr.h"
 #include "mdr_mdrd.h"
@@ -15,6 +17,10 @@
  *
  * If the client did not have a cert, it will return a CERTFAIL error.
  */
+
+X509_STORE     *store;
+X509_STORE_CTX *ctx;
+
 int
 main(int argc, char **argv)
 {
@@ -25,6 +31,8 @@ main(int argc, char **argv)
 	int               fd;
 	X509             *peer_cert = NULL;
 	struct sigaction  act;
+	X509_STORE_CTX   *ctx;
+	X509_LOOKUP      *lookup;
 
 	/*
 	 * We're disabling TERM/INT because we instead rely on
@@ -36,6 +44,33 @@ main(int argc, char **argv)
 	if (sigaction(SIGINT, &act, NULL) == -1 ||
 	    sigaction(SIGTERM, &act, NULL) == -1)
 		return -1;
+
+	openlog("mdrd_backend_echo", LOG_PID|LOG_PERROR, LOG_USER);
+
+	if ((ctx = X509_STORE_CTX_new()) == NULL) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+
+	if ((store = X509_STORE_new()) == NULL)
+		err(1, "X509_STORE_new");
+
+	if ((lookup = X509_STORE_add_lookup(store,
+	    X509_LOOKUP_file())) == NULL) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+
+	if (!X509_load_cert_file(lookup, "/home/plalonde/prog/mdr/ca/overnet.pem",
+	    X509_FILETYPE_PEM)) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
+
+	if (!X509_load_crl_file(lookup, "/home/plalonde/prog/mdr/ca/overnet.crl", X509_FILETYPE_PEM)) {
+		ERR_print_errors_fp(stderr);
+		exit(1);
+	}
 
 	while ((r = mdr_unpack_from_fd(&m, 0, buf, sizeof(buf))) > 0) {
 		if (r == MDR_FAIL)
@@ -61,7 +96,20 @@ main(int argc, char **argv)
 			    MDRD_BERESP_F_CLOSE, NULL) == MDR_FAIL)
 				err(1, "mdrd_pack_beresp");
 		} else {
+			if (!X509_STORE_CTX_init(ctx, store, peer_cert, NULL)) {
+				ERR_print_errors_fp(stderr);
+				exit(1);
+			}
+
+			if ((r = X509_verify_cert(ctx)) <= 0) {
+				syslog(LOG_ERR, "X509_verify_cert: %s",
+				    ERR_error_string(ERR_get_error(), NULL));
+				exit(1);
+			}
+			X509_STORE_CTX_cleanup(ctx);
+
 			X509_free(peer_cert);
+
 			if (mdrd_pack_beresp(&m, buf, sizeof(buf), id, fd,
 			    MDRD_ST_OK, MDRD_BERESP_F_MSG,
 			    &msg) == MDR_FAIL)
@@ -71,5 +119,7 @@ main(int argc, char **argv)
 		if (write(1, mdr_buf(&m), mdr_size(&m)) < mdr_size(&m))
 			err(1, "writeall");
 	}
+	X509_STORE_CTX_free(ctx);
+	X509_STORE_free(store);
 	return 0;
 }
