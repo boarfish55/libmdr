2f8d281737a08363aa25d84348edd4e1e4af56b7
Pascal Lalonde <plalonde@overnet.ca>
2025-12-29 21:03:41 -0500

    Make openssl .cnf more generalized, remove refs to overnet

--- bootstrap_ca.sh
+++ bootstrap_ca.sh
@@ -2,18 +2,20 @@
 
 set -e
 
+DOMAIN=overnet.ca
+ORG=Overnet
+
 rm -rf ca
-mkdir -p ca/certs ca/private
-chmod 700 ca/private
+mkdir -p ca/certs
 echo "01" > ca/serial
 touch ca/index.txt
 
 # Create self-signed root certificate
 openssl req -x509 -nodes -config mdrd_ssl.cnf -newkey ed25519 \
-	-keyout ca/private/overnet_key.pem \
-	-out ca/overnet.pem -outform PEM -days 3650 \
+	-keyout ca/key.pem \
+	-out ca/root.pem -outform PEM -days 3650 \
 	-extensions root_ext \
-	-subj "/emailAddress=cert@overnet.ca/O=Overnet/CN=Overnet CA"
+	-subj "/emailAddress=cert@$DOMAIN/O=$ORG/CN=$ORG CA"
 
 # Create a "client1" cert request
 rm -f client1/*
@@ -21,12 +23,12 @@ mkdir -p client1
 openssl req -nodes -config mdrd_ssl.cnf -newkey ed25519 \
 	-keyout client1/key.pem -keyform PEM \
 	-out client1/req.pem -outform PEM \
-	-subj "/O=Overnet/CN=client1.overnet.ca" \
-	-addext "subjectAltName = DNS:client1.overnet.ca"
+	-subj "/O=$ORG/CN=client1.$DOMAIN" \
+	-addext "subjectAltName = DNS:client1.$DOMAIN"
 
 # Sign client1 cert & verify
 yes | openssl ca -config mdrd_ssl.cnf -in client1/req.pem -out client1/cert.pem
-openssl verify -CAfile ca/overnet.pem client1/cert.pem
+openssl verify -CAfile ca/root.pem client1/cert.pem
 
 # Create a "client2" cert request
 rm -f client2/*
@@ -34,20 +36,20 @@ mkdir -p client2
 openssl req -nodes -config mdrd_ssl.cnf -newkey ed25519 \
 	-keyout client2/key.pem -keyform PEM \
 	-out client2/req.pem -outform PEM \
-	-subj "/O=Overnet/CN=client2.overnet.ca" \
-	-addext "subjectAltName = DNS:client2.overnet.ca"
+	-subj "/O=$ORG/CN=client2.$DOMAIN" \
+	-addext "subjectAltName = DNS:client2.$DOMAIN"
 
 # Sign and revoke client2 cert
 yes | openssl ca -config mdrd_ssl.cnf -in client2/req.pem -out client2/cert.pem
-openssl verify -CAfile ca/overnet.pem client2/cert.pem
+openssl verify -CAfile ca/root.pem client2/cert.pem
 openssl ca -config mdrd_ssl.cnf -revoke client2/cert.pem
 
 # Generate CRL
-openssl ca -config mdrd_ssl.cnf -gencrl -out ca/overnet.crl
-openssl verify -CAfile ca/overnet.pem -CRLfile ca/overnet.crl \
+openssl ca -config mdrd_ssl.cnf -gencrl -out ca/root.crl
+openssl verify -CAfile ca/root.pem -CRLfile ca/root.crl \
 	-crl_check client2/cert.pem || true
 
 # View it and verify signature
-openssl crl -in ca/overnet.crl -text -noout -CAfile ca/overnet.pem
+openssl crl -in ca/root.crl -text -noout -CAfile ca/root.pem
 
 echo "All good!"
