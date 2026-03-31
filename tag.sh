c03e45d0a1efdb8d11844492cf812bdaf0a95e8f
Pascal Lalonde <plalonde@overnet.ca>
2026-03-30 20:07:36 -0400

    Add tag script for convenience

---
+++ tag.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+set -e
+
+cmd="$1"
+
+cur=$(git describe --abbrev=0)
+cur=${cur#v}
+
+major=$(echo $cur | cut -d. -f 1)
+minor=$(echo $cur | cut -d. -f 2)
+patch=$(echo $cur | cut -d. -f 3)
+
+if [ "$major" -gt "$(echo $cur | cut -d. -f 1)" ]; then
+	minor=0
+	patch=0
+else
+	case $cmd in
+		major)
+			major=$(($major + 1))
+			minor=0
+			patch=0
+			;;
+		minor)
+			minor=$(($minor + 1))
+			patch=0
+			;;
+		patch)
+			patch=$(($patch + 1))
+			;;
+		*)
+			echo "Usage: $(basename $0) <major|minor|patch>"
+			exit 2
+			;;
+	esac
+fi
+
+echo "$cur => $major.$minor.$patch"
+
+read -p "Tag it? (y/N) " RESP
+
+if [ "$RESP" = "y" -o "$RESP" = "Y" ]; then
+	git tag -m "v$major.$minor.$patch" "$major.$minor.$patch"
+fi
+
+exit 0
