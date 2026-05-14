9a6dd30eb32ccd2627c4e19765ffbfb135d986ee
Pascal Lalonde <plalonde@overnet.ca>
2026-05-14 19:21:37 -0400

    Fix dirent underflow

--- mdrd.c
+++ mdrd.c
@@ -937,7 +937,8 @@ scan_crls(struct crl_stat *dst, size_t dst_sz, X509_STORE *store,
 			if (de->d_type != DT_REG)
 				continue;
 			de_len = strlen(de->d_name);
-			if (strcmp(de->d_name + (de_len - 4), ".crl") != 0)
+			if (de_len < 4 ||
+			    strcmp(de->d_name + (de_len - 4), ".crl") != 0)
 				continue;
 
 			snprintf(crl_path, sizeof(crl_path), "%s/%s",
