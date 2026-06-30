e160682f76200a27b18d724a380860b47ceb7e6d
Pascal Lalonde <plalonde@overnet.ca>
2026-06-30 14:23:22 -0400

    Make debian happy

--- debian/refresh_symbols.sh
+++ debian/refresh_symbols.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cd ..
+cd "$(dirname "$0")/.."
+make libmdr.so libflatconf.so
 version=$(dpkg-parsechangelog -SVersion | cut -d- -f 1)
 dpkg-gensymbols -v${version} -plibmdr0 -elibmdr.so -elibflatconf.so \
 	-Odebian/libmdr0.symbols
